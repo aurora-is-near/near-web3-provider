a9633b04057da7b331ad0d341e9ca8dd69ef050d
Barbara Liau <barbara@liau.io>
2020-02-03 19:07:29 -0700

    Initial commit

---
+++ near_to_eth_objects.js
@@ -0,0 +1,109 @@
+/**
+ * NEAR chunks mapped to ETH objects and vice versa
+ */
+
+const utils = require('./utils');
+
+const nearToEth = {};
+
+/**
+ * Formats NEAR chunks to ETH transaction objects
+ * @param {Array} block block
+ * @param {Boolean} returnTxObjects (optional) default false. if true,
+ * return entire transaction object, otherwise just hashes
+ * @returns {Array} returns array of tx hashes or full tx object
+ */
+nearToEth._formatChunksToTx = function(block, returnTxObjects) {
+	const hasTxs = block.chunks.length > 0;
+	let transactions = [];
+
+	if (hasTxs && !returnTxObjects) {
+		// Return tx hashes (default)
+		transactions = block.chunks.map((c) => utils.base58ToHex(c.chunk_hash));
+	} else if (hasTxs && returnTxObjects) {
+		// Return transaction object if requested and txs exist
+		const blockHeaderHash = block.header.hash;
+		transactions = block.chunks.map((c) => this.transactionObj(c, blockHeaderHash));
+	}
+
+	return transactions;
+}
+
+/**
+ * Maps NEAR chunk to ETH Transaction Object
+ * @param {Object} chunk NEAR transaction chunk
+ * @param {Hex} blockHeaderHash
+ * @returns {object} returns ETH transaction object
+ * @example eth.transactionObject(chunk, blockHeaderHash)
+ */
+nearToEth.transactionObj = function(chunk, blockHeaderHash) {
+	return {
+		hash: utils.base58ToHex(chunk.chunk_hash),
+		nonce: '0x1',
+		blockHash: utils.base58ToHex(blockHeaderHash),
+		blockNumber: utils.decToHex(chunk.height_included),
+
+		// TODO: Don't know how to get these values
+		transactionIndex: '0x1',
+		from: '0xFb4d271F3056aAF8Bcf8aeB00b5cb4B6C02c7368',
+		to: '0xFb4d271F3056aAF8Bcf8aeB00b5cb4B6C02c7368',
+		gas: '0xffffffff',
+		gasPrice: '0x4a817c800',
+		input: '0x',
+		value: '0x',
+		v: '0x25',
+		r: '0x1b5e176d927f8e9ab405058b2d2457392da3e20f328b16ddabcebc33eaac5fea',
+		s: '0x4ba69724e8f69de52f0125ad8b3c5c2cef33019bac3249e2c0a2192766d1721c'
+	};
+}
+
+/**
+ * Maps NEAR Block to ETH Block Object
+ * @param {Object} block NEAR block
+ * @param {Boolean} returnTxObjects (optional) default false. if true, return
+ * entire transaction object, other just hashes
+ * @returns {Object} returns ETH block object
+ */
+nearToEth.blockObj = function(block, returnTxObjects) {
+	const { header } = block;
+
+	return {
+		number: utils.decToHex(header.height),
+		hash: utils.base58ToHex(header.hash),
+		parentHash: utils.base58ToHex(header.prev_hash),
+		nonce: null,
+		// sha3Uncles: '',
+		// logsBloom: '',
+		transactionsRoot: utils.base58ToHex(header.chunk_tx_root),
+		// stateRoot: '',
+		// miner: '',
+		// difficulty: null,
+		// totalDifficulty: null,
+		// extraData: '',
+		// size: null,
+		// TODO: gas limit
+		gasLimit: "0xffffffffff",
+		gasUsed: null,
+		timestamp: utils.convertTimestamp(header.timestamp),
+		transactions: this._formatChunksToTx(block, returnTxObjects),
+		uncles: ['']
+	};
+};
+
+/**
+ * ETH Sync Object
+ */
+nearToEth.syncObj = function() {
+	return {
+		// TODO: Not sure how to get this
+		startingBlock: '0x0',
+		currentBlock: utils.decToHex(sync_info.latest_block_height),
+		// TODO: Not sure how to get this
+		highestBlock: '0x0',
+		// TODO: The following are not listed in the RPC docs but are expected in web3
+		knownStates: '0x0',
+		pulledStates: '0x0'
+	};
+}
+
+module.exports = nearToEth;
---
+++ utils.js
@@ -0,0 +1,110 @@
+const assert = require('bsert');
+const bs58 = require('bs58');
+
+const utils = {};
+
+/**
+ * Remove 0x if prepended
+ * @param {string} value value to check and modify
+ * @return {string} string without 0x
+ */
+utils.remove0x = function(value) {
+  if (value.slice(0, 2) === '0x') {
+    return value.slice(2);
+  } else {
+    return value;
+  }
+};
+
+/**
+ * Checks if string is hex
+ * @param {string} value value to check
+ * @returns {boolean} true if value is hex, false if not
+ */
+utils.isHex = function(value) {
+  assert(typeof value === 'string', 'isHex: must pass in string');
+  const hexTest = /^(0[xX])?[A-Fa-f0-9]+$/;
+  return hexTest.test(value);
+}
+
+/**
+ * Converts number to hex
+ * @param {number} value number to convert to hex
+ * @returns {string} hex string equivalent of number
+ */
+utils.decToHex = function(value) {
+  assert(typeof value === 'number', 'decToHex: must pass in number');
+
+  return '0x' + value.toString(16);
+};
+
+/**
+ * Converts hex to number
+ * @param {string} value hex string to convert to number
+ * @returns {number} number equivalent of hex string
+ */
+utils.hexToDec = function(value) {
+  assert(typeof value === 'string', 'hexToDec: must pass in hex string');
+  assert(this.isHex(value), 'hexToDec: must pass in hex string');
+
+  return parseInt(value.slice(2), 16);
+};
+
+/**
+ * Converts base58 object to hex string
+ * @param {string|array|buffer|arrayBuffer} value base58 object
+ * @returns {string} hex string equivalent of base58 object
+ */
+utils.base58ToHex = function(value) {
+  return '0x' + Buffer.from(bs58.decode(value)).toString('hex');
+};
+
+/**
+ * Converts hex string to base58 string
+ * @param {string} value hex string
+ * @returns {string} returns base58 string equivalent of hex string
+ */
+utils.hexToBase58 = function(value) {
+  console.log({value})
+  assert(typeof value === 'string', 'hexToBase58: must pass in hex string');
+  assert(this.isHex(value), 'hexToBase58: must pass in hex string');
+  value = this.remove0x(value);
+
+  return bs58.encode(Buffer.from(value, 'hex'));
+};
+
+/**
+ * Converts base64 object to hex string
+ * @param {string|array|buffer|arrayBuffer} value base64 object
+ * @returns {string} hex string equivalent of base64 object
+ */
+utils.base64ToHex = function(value) {
+  return '0x' + Buffer.from(value, 'base64').toString('hex');
+};
+
+/**
+ * Converts base64 object to string
+ * @param {string|array|buffer|arrayBuffer} value base64 object
+ * @returns {string} string equivalent of base64 object
+ */
+utils.base64ToString = function(value) {
+  return Buffer.from(value, 'base64').toString();
+};
+
+/**
+ * Convert timestamp in NEAR to hex
+ * @param {number} value NEAR timestamp
+ * @returns {string} hex string equivalent of timestamp
+ */
+utils.convertTimestamp = function(value) {
+  assert(typeof value === 'number', 'convertTimestamp: must pass in number');
+  // NB: NEAR timestamps need to be divided by 1000000 to be converted to a value that produces a valid new Date(). However, this value is now a decimal, which cannot be converted to a valid hex String. Passing this value into new Date() and then calling getTime() will yield a rounded epoch time that can then be converted to valid hex.
+  // Example with NEAR block number 1221180 with timestamp 1580771932817928262.
+  // Dividing this by 1000000 yields 1580771932817.9282 which translates to a date of 'Mon Feb 03 2020 16:18:52 GMT-0700'.Calling getTime() gives 1580771932000 which can then be converted to hex 0x1700d597760.
+  // If we convert the original timestamp 1580771932817928262 to hex, it yields 0x15f007b296853000. Attempting to pass this through to web3 results in the error: 'Number can only safely store up to 53 bits'
+  const divider = 1000000;
+  const roundedTime = new Date(value / divider).getTime();
+  return this.decToHex(roundedTime);
+};
+
+module.exports = utils;
