5caf5727be9da2bc654c40de7f6c7d72ce531dc6
Barbara Liau <barbara@liau.io>
2020-07-21 11:07:36 -0600

    Update path

--- test/config.js
+++ test/config.js
@@ -1,9 +1,9 @@
 const path = require('path');
-
+const keyPath =
 module.exports = {
     local: {
         networkId: 'local',
         nodeUrl: 'http://localhost:3030',
-        keyPath: path.resolve('../nearcore/testdir/validator_key.json')
+        keyPath: path.resolve('../nearcore/testdir/.near/validator_key.json')
     }
 };
