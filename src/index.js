aurora-is-near/near-web3-provider

commit 908ef1014f88ce76585b89b2be3b0035db983432

Move the code from #35

src/index.js
@@ -77,13 +77,35 @@ class NearProvider {
     }
 
     /**
-     * Calls a block and fills it up
+     * TODO: Make return params into options object for better readability
+     * TODO: Return consistent type: either an Array or an Object
+     *
+     * Gets a Near block and formats it according to web3 provider requirements
+     * @param {Object} blockId Block height, block hash, or block
+     * finality (enum: 'final', 'near-final', 'optimistic')
+     * @param {Boolean} shouldReturnTxObjects Indicates whether this method
+     * should return a block with tx objects. Default false.
+     * @param {Boolean} shouldReturnNearBlock Indicates whether this method
+     * should return an unformatted Near block. Default false.
+     * @returns {Object|Array}
      */
-    async _getBlock(blockId, returnTxObjects, returnNearBlock) {
-        const block = await this.nearProvider.block({ blockId });
-        const fullBlock = await nearToEth.blockObj(block, returnTxObjects, this.nearProvider);
+    async _getBlock(blockId, shouldReturnTxObjects, shouldReturnNearBlock) {
+        const finalityEnums = ['final', 'near-final', 'optimistic'];
+        const isFinalityType = typeof blockId === 'string'
+            ? finalityEnums.find((f) => f === blockId)
+            : false;
+        const query = {};
+        if (typeof blockId === 'number') {
+            query.blockId = blockId;
+        } else if (isFinalityType) {
+            query.finality = blockId;
+        } else {
+            query.blockQuery = blockId;
+        }
+        const block = await this.nearProvider.block(query);
+        const fullBlock = await nearToEth.blockObj(block, shouldReturnTxObjects, this.nearProvider);
 
-        if (returnNearBlock) {
+        if (shouldReturnNearBlock) {
             return [fullBlock, block];
         }
         return fullBlock;

test/helpers.js
@@ -1,0 +1,22 @@
+/**
+ * Helpers
+ */
+
+const utils = require('../src/utils');
+
+async function getLatestBlockInfo(nearProvider) {
+    const { sync_info } = await nearProvider.status();
+    const { latest_block_hash, latest_block_height } = sync_info;
+    const block = {
+        blockHash: utils.base58ToHex(latest_block_hash),
+        blockHeight: latest_block_height
+    };
+
+    return block;
+}
+
+async function waitForABlock() {
+    return await new Promise((r) => setTimeout(r, 1000));
+}
+
+module.exports = { getLatestBlockInfo, waitForABlock };

test/near_provider.test.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const web3 = require('web3');
 const bn = web3.utils.BN;
 const { NearProvider, nearWeb3Extensions, nearAPI, utils } = require('../src/index');
+const { waitForABlock, getLatestBlockInfo } = require('./helpers');
 
 let source = fs.readFileSync('./test/build/contracts/ZombieAttack.json');
 const zombieCode = JSON.parse(source)['bytecode'];
@@ -569,7 +570,7 @@ describe('\n---- PROVIDER ----', () => {
         const base58TxHash = 'ByGDjvYxVZDxv69c86tFCFDRnJqK4zvj9uz4QVR4bH4P';
 
         beforeAll(withWeb3(async (web) => {
-            const newBlock = await getLatestBlockInfo();
+            const newBlock = await getLatestBlockInfo(testNearProvider);
             blockHash = newBlock.blockHash;
             blockHeight = newBlock.blockHeight;
 
@@ -945,21 +946,3 @@ describe('\n---- PROVIDER ----', () => {
         });
     });
 });
-
-/**
- * Helpers
- */
-async function getLatestBlockInfo () {
-    const { sync_info } = await testNearProvider.status();
-    const { latest_block_hash, latest_block_height } = sync_info;
-    const block = {
-        blockHash: utils.base58ToHex(latest_block_hash),
-        blockHeight: latest_block_height
-    };
-
-    return block;
-}
-
-async function waitForABlock () {
-    return await new Promise((r) => setTimeout(r, 1000));
-}

commit e75cbcea1d64af1db27d51b5e1d0e5e0413c8fb2

Fix regression introduced in 908ef10.

src/index.js
@@ -91,16 +91,11 @@ class NearProvider {
      */
     async _getBlock(blockId, shouldReturnTxObjects, shouldReturnNearBlock) {
         const finalityEnums = ['final', 'near-final', 'optimistic'];
-        const isFinalityType = typeof blockId === 'string'
-            ? finalityEnums.find((f) => f === blockId)
-            : false;
-        const query = {};
-        if (typeof blockId === 'number') {
-            query.blockId = blockId;
-        } else if (isFinalityType) {
+        const query = {}; // See: https://near.github.io/near-api-js/classes/_providers_provider_.provider.html#block
+        if (typeof blockId === 'string' && finalityEnums.find((f) => f === blockId)) {
             query.finality = blockId;
         } else {
-            query.blockQuery = blockId;
+            query.blockId = blockId;
         }
         const block = await this.nearProvider.block(query);
         const fullBlock = await nearToEth.blockObj(block, shouldReturnTxObjects, this.nearProvider);